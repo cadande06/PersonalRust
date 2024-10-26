708c322597bbdaffa18cb3e5d44e7f3ff239fd51
Chioma Adande <chiomadande@gmail.com>
2024-10-26 12:55:18 +0100

    first commit

---
+++ begin_work/first.rs
@@ -0,0 +1,13 @@
+fn main(){
+    // main() must be used at the beginning of every rust file
+    //println! is a macro used to run input on the terminalOS or cmd
+    println!("Hello World");
+    /*
+     * This is another type of comment, a block comment. In general,
+     * line comments are the recommended comment style. But block comments
+     * are extremely useful for temporarily disabling chunks of code.
+     * /* Block comments can be /* nested, */ */ so it takes only a few
+     * keystrokes to comment out everything in this main() function.
+     */
+    
+}
