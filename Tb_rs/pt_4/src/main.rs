ad90096c3386eb42149f5a578b00577930c63fdc
Chioma Adande <chiomadande@gmail.com>
2024-11-13 11:08:48 +0100

    first commit

---
+++ Tb_rs/pt_4/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    //MEMORY MANAGEMENT 
+    /*
+    STACK AND HEAP- PART of the RAM(random access memory)
+    Whenever we run a program, we need some space on the RAM to store the data of our program and we want it to be easily accessible don't want duplicate data.Also, when we aren't using a variable or we have already used it and have no use of it anymore, we want it removed from RAM.
+    Wehave Stack and Heap in Rust
+    RUST STACK - the most recent walua added would be the first to be removed because it's at the top. LIFO - Last In First Out. FILO - First In Last Out. Stach is the fastest piece of RAM in Rust.We try to use the stack as much as possible.
+    RUST HEAP - heap is used when we need more space. If you need to allocate a large block of memory (e.g. a large array, or a big struct), and you need to keep that variable around a long time (like a global), use the heap.
+    */
+}
---
+++ Tb_rs/pt_4/target/debug/deps/pt_4-53644f57ba223513.d
@@ -0,0 +1,5 @@
+/Users/chiomadande/Documents/PersonalRust/Tb_rs/pt_4/target/debug/deps/libpt_4-53644f57ba223513.rmeta: src/main.rs
+
+/Users/chiomadande/Documents/PersonalRust/Tb_rs/pt_4/target/debug/deps/pt_4-53644f57ba223513.d: src/main.rs
+
+src/main.rs:
